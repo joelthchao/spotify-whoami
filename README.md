085c8de78b72b3d391e31eba801b050ffa035ebf
Joel <joelthchao@gmail.com>
2020-08-13 12:27:03 +0800

    add taste comparison to region

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## TODO
 
 - [ ] Simplify authentication
-- [ ] Add taste comparison target (e.g. country's popular songs)
+- [V] Add taste comparison target (e.g. country's popular songs)
 - [ ] GUI instead of command line
--- spotify_whoami/reader.py
+++ spotify_whoami/reader.py
@@ -34,7 +34,12 @@ def read(token):
         raise Exception('No track ids are found.')
 
     audio_features = sp.audio_features(track_ids)
+    df = make_feature_df(audio_features, popularities)
 
+    return df
+
+
+def make_feature_df(audio_features, popularities):
     feature_keys = [
         ('danceability', 0, 1),
         ('energy', 0, 1),
@@ -48,25 +53,54 @@ def read(token):
     ]
 
     data = []
-    for audio_feature in audio_features:
+    for audio_feature, popularity in zip(audio_features, popularities):
+        data.append(('popularity', popularity))
         for key, low, high in feature_keys:
             val = (audio_feature[key] - low) / (high - low)
             data.append((key, val))
     df = pd.DataFrame(data, columns=['feature', 'value'])
-
     return df
 
 
-def plot(df):
+def read_feature_average(token, region='tw'):
+    sp = spotipy.Spotify(auth=token)
+    res = sp.category_playlists('toplists', 'tw')
+    toplist_id = res['playlists']['items'][0]['id']  # region's top track usually is the first item
+    res = sp.playlist(toplist_id)
+
+    popularities = []
+    track_ids = []
+    for item in res['tracks']['items']:
+        track = item['track']
+        popularities.append(track['popularity'] / 100)
+        track_ids.append(track['id'])
+    audio_features = sp.audio_features(track_ids)
+
+    df = make_feature_df(audio_features, popularities)
+    feature_avgs = []
+    for key in set(df['feature']):
+        avg_value = df[df['feature'] == key]['value'].mean()
+        feature_avgs.append((key, avg_value))
+    avg_df = pd.DataFrame(feature_avgs, columns=['feature', 'value'])
+
+    return avg_df
+
+
+def plot(df, avg_df):
     f, ax = plt.subplots()
-    sns.stripplot(x='feature', y='value', data=df, alpha=0.25)
-    sns.pointplot(x='feature', y='value', data=df, join=False, palette='dark',
+    order = sorted(set(df['feature']))
+    sns.stripplot(x='feature', y='value', order=order, data=df, alpha=0.2)
+    sns.pointplot(x='feature', y='value', order=order, data=df, join=False, palette='dark',
                   markers='D', scale=0.5, ci=None)
+    sns.pointplot(x='feature', y='value', order=order, data=avg_df, join=False, palette='dark',
+                  markers='*', scale=0.7, ci=None)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=30, horizontalalignment='right', fontsize=6)
+    ax.set_title('Spotify Taste (♦: Your Avg. ☆: Region Avg.)')
     plt.show()
 
 
 if __name__ == '__main__':
     token = login()
     df = read(token)
-    plot(df)
+    avg_df = read_feature_average(token)
+    plot(df, avg_df)
